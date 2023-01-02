94edee7141e4dc568ca2473894a16426c2cb8330
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2023-01-02 12:33:54 +0000

    asynchronus

---
+++ it2_2.js
@@ -0,0 +1,15 @@
+/*2.2 Convierte la siguiente función con un fetch utilizando async-await. 
+Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+function getCharacters () {
+    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters();*/
+
+async function getCharacters () {
+    const res = await fetch('https://rickandmortyapi.com/api/character');
+    const characters = await res.json();
+    console.log(characters);
+}
+
+getCharacters();
